b074699274111cf0f06b4a5f77c8828e3266700f
Engin Yenice <enginyenice2626@gmail.com>
2020-06-24 02:25:09 +0300

    Login tasarımı eklendi.  Giriş çıkış işlemleri için AccountController eklendi.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TelefonSatis.Data;
+using TelefonSatis.Models;
+
+namespace TelefonSatis.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly DataBaseContex _context;
+
+        public AccountController(DataBaseContex context)
+        {
+            _context = context;
+        }
+
+
+        // GET: Users/Create
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string Mail,string Password)
+        {
+            Console.WriteLine(Mail, Password);
+
+
+            return Login();
+        }
+
+
+
+    }
+}
