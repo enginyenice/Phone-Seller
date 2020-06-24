cdb511e779e93fc1b4bd716459a125f1b34cf472
Engin Yenice <enginyenice2626@gmail.com>
2020-06-24 03:10:49 +0300

    Session Add

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿/*
+ * TODO:Login işlemi hatalıysa geri dönüşte hata verdir..
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Session;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using TelefonSatis.Data;
 using TelefonSatis.Models;
+using Microsoft.AspNetCore.Session;
+using Microsoft.AspNetCore.Http;
 
 namespace TelefonSatis.Controllers
 {
@@ -31,10 +39,30 @@ namespace TelefonSatis.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Login(string Mail,string Password)
         {
-            Console.WriteLine(Mail, Password);
+            int result = 0;
+            result = _context.Users.Where(u => u.Mail == Mail && u.Password == Password).Count();
+            
 
+            if(result > 0)
+            {
+                //Kullanici Bilgilerini Cek
+                string mail         = Mail;
+                string userName     = "";
+                string password     = "";
+                string permission   = "";
 
-            return Login();
+                //HttpContext.Session.SetString("Mail", mail);
+                //HttpContext.Session.SetString("userName", userName);
+                //HttpContext.Session.SetString("Permission", permission);
+                return View("Index");
+            }else
+            {
+
+                return View("Login");
+            }
+
+            
+            return View("Login");
         }
 
 
